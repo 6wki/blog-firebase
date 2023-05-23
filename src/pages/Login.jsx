c63753522b217fa32bb8180988048a9eed67ae4e
chouki.chikhi <mhcc213@gmail.com>
2023-05-23 21:18:46 +0200

    updated login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = ({ setIsAuth }) => {
       <div className="loginContainer">
         <img src={logo} className="logoLogin" />
         <p>Please Sign In with Google to Continue</p>
-        <button class="loginBtn">
+        <button className="loginBtn" onClick={signInWithGoogle}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             preserveAspectRatio="xMidYMid"
