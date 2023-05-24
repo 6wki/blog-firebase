b3bd1c93fc20062fa698f1ed19c1aaae97a5a008
chouki.chikhi <mhcc213@gmail.com>
2023-05-24 15:20:00 +0200

    updated feedback section

--- src/pages/LandingPage/Feedback.jsx
+++ src/pages/LandingPage/Feedback.jsx
@@ -1,31 +1,32 @@
 import React from "react";
+import headline from "../../assets/imgs/fdbackHeadline.svg";
+import freedom from "../../assets/imgs/freedom.svg";
+import lfth from "../../assets/imgs/hand-left.svg";
+import rth from "../../assets/imgs/hand-right.svg";
+import bg3 from "../../assets/imgs/bg3.svg";
+import infoBg from "../../assets/imgs/infoBg.svg";
 
 const Feedback = () => {
   return (
     <section id="feedback" className="feedback sec">
-      <form className="feedbackCard glass">
-        <label htmlFor="nameLogin">Full Name</label>
-        <input
-          type="text"
-          id="nameLogin"
-          placeholder="Enter your name"
-          required
-        />
-        <label htmlFor="emailLogin">Email</label>
-        <input
-          type="text"
-          id="emailLogin"
-          placeholder="Enter your name"
-          required
-        />
-        <label htmlFor="feedbackArea">Your Feedback</label>
-        <textarea
-          type="text"
-          id="feedbackArea"
-          placeholder="This is the coolest website!!"
-          required
-        />
-      </form>
+      <img src={bg3} className="bgFd" />
+      <div className="feedbackContainer glass splitContainer">
+        <div className="leftFd split">
+          <img src={headline} className="hdFdback" />
+          <img src={freedom} className="freedom" />
+          <img src={lfth} className="leftHand" />
+          <img src={rth} className="rightHand" />
+          <div className="circles">
+            <div className="circle c1"></div>
+            <div className="circle c2"></div>
+            <div className="circle c3"></div>
+          </div>
+        </div>
+        <div className="rightFd split">
+          <p></p>
+          <img src={infoBg} className="infoBg" />
+        </div>
+      </div>
     </section>
   );
 };
